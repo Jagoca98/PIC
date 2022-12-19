d8cd631fbf26bcce4b10ed0e130b16149340e35d
Jaime <jagoca98@gmail.com>
2022-12-19 13:53:56 +0100

    a

--- main2.py
+++ main2.py
@@ -6,7 +6,7 @@ import math
 
 class PIC:
     def thread_function(self, name):
-        cap = cv2.VideoCapture('http://192.168.1.64:8080/video')
+        # cap = cv2.VideoCapture('http://192.168.1.64:8080/video')
         self.img = cv2.imread('assets/ss.jpeg')
         # while(True):
         #     ret, frame = cap.read()
@@ -30,51 +30,98 @@ class PIC:
 
             # create a CLAHE object (Arguments are optional).
             clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-            imgBN = clahe.apply(imgBN)
-            imthresh = imgBN
-            ret,imthresh = cv2.threshold(imgBN,127,255,cv2.THRESH_BINARY)
-            ret,imthresh = cv2.threshold(imgBN,200,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-
-            # imthresh = cv2.medianBlur(imthresh,9)
-
+            imgBN = clahe.apply(imgBN)            
             # Aplicar suavizado Gaussiano
-            # gauss = cv2.GaussianBlur(imthresh, (5,5), 0)
-        
+            gauss = cv2.GaussianBlur(self.img, (5,5), 0)
             
+            # cv2.imshow("suavizado", gauss)
+
+            # imthresh = imgBN
+            ret,imthresh = cv2.threshold(gauss,127,255,cv2.THRESH_BINARY)
+            # ret,imthresh = cv2.threshold(gauss,200,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
             # Detectamos los bordes con Canny
-            # canny = cv2.Canny(gauss, 50, 150)
-        
-            # cdstP = cv2.cvtColor(canny, cv2.COLOR_GRAY2BGR)
+            canny = cv2.Canny(imthresh, 50, 150)
+
+            # Marker labelling
+            marker_count, markers = cv2.connectedComponents(canny)
+            # Add one to all labels so that sure background is not 0, but 1
+            markers = markers+1
+            # Now, mark the region of unknown with zero
+            # markers[unknown==255] = 0
+            segmented = cv2.watershed(self.img,markers)
+            # END of original watershed example
             
-            # linesP = cv2.HoughLinesP(canny, 1, np.pi / 180, 50, None, 50, 10)
-            
-            # if linesP is not None:
-            #     for i in range(0, len(linesP)):
-            #         l = linesP[i][0]
-            #         cv2.line(cdstP, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+
+            output = np.zeros_like(self.img)
+            output2 = self.img.copy()
 
 
-            corners = cv2.goodFeaturesToTrack(imthresh, 4, 0.4, 10, useHarrisDetector=True, k=0.04)
+            kernel = np.ones((3,3),np.uint8)
+            dilation = cv2.dilate(canny.copy(),kernel,iterations = 2)
+            opening = cv2.morphologyEx(dilation,cv2.MORPH_OPEN,kernel, iterations = 1)
+            imthresh = cv2.medianBlur(opening, 9)
+
+            ##################################################################
+
+            opening = cv2.morphologyEx(imthresh,cv2.MORPH_OPEN,kernel, iterations = 1)
+            # sure background area
+            sure_bg = cv2.dilate(opening,kernel,iterations=4)
+            # Finding sure foreground area
+            dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+            dist_transform = cv2.distanceTransform(cv2.dilate(opening,kernel,iterations=3),cv2.DIST_L2,5)
+            ret, sure_fg = cv2.threshold(dist_transform, 0.0*dist_transform.max(),255,0)
+            # Finding unknown region
+            sure_fg = np.uint8(sure_fg)
+            unknown = cv2.subtract(sure_bg,sure_fg)
+            # Marker labelling
+            marker_count, markers = cv2.connectedComponents(sure_fg)
+            # Add one to all labels so that sure background is not 0, but 1
+            markers = markers+1
+            if marker_count >= 5: marker_count=5
+            # Now, mark the region of unknown with zero
+            markers[unknown==255] = 0
+            segmented = cv2.watershed(self.img,markers)
+            # END of original watershed example
             
+            output = np.zeros_like(self.img)
+            output2 = self.img.copy()
+            output3 = self.img.copy()
+            
+            area_array = []
+
+            # Iterate over all non-background labels
+            for i in range(2, marker_count + 1):
+                mask = np.where(segmented==i, np.uint8(255), np.uint8(0))
+                x,y,w,h = cv2.boundingRect(mask)
+                area = cv2.countNonZero(mask[y:y+h,x:x+w])
+                area_array.append(area)
+                # print("Label %d at (%d, %d) size (%d x %d) area %d pixels" % (i,x,y,w,h,area))
+
+                # Visualize
+                color = np.uint8(np.random.randint(0, 255 + 1)).tolist()
+                output[mask!=0] = color
+                cv2.rectangle(output2, (x,y), (x+w,y+h), color, 1)
+            
+            index = area_array.index(max(area_array))+2
+            print(index)
+            mask = np.where(segmented==index, np.uint8(255), np.uint8(0))
+            # color = np.uint8(np.random.randint(0, 255 + 1)).tolist()
+            output3[:,:] = 0
+            output3[mask!=0] = 255
+            cv2.imshow('a', output3)
+            #####################################################################
+
+            # cv2.imshow('aa', opening)
+
+            corners = cv2.goodFeaturesToTrack(opening, 4, 0.01, 10)
             corners = np.int0(corners)
-
-            num = 0
+            
             for i in corners:
                 x,y = i.ravel()
-                cv2.circle(imthresh, (x,y), 3, 255, -1)
-                num = num+1
-
-
-            if num >= 4:
-                pts1 = np.float32([[corners[0].ravel()], [corners[1].ravel()], [corners[2].ravel()], [corners[3].ravel()]])
-                pts2 = np.float32([[0,0], [width, 0], [height, 0], [width, height]])
-                matrix = cv2.getPerspectiveTransform(pts1,pts2)
-                img = cv2.warpPerspective(imthresh, matrix,(width, height))
-
-            cv2.imshow("Crop", img)
-            cv2.imshow('Imagen', imthresh)
+                cv2.circle(output2, (x,y), 3, 255, -1)
             
-
+            # cv2.imshow('corner', output2)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 self.kill = True
